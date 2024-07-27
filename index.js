c0829a6930825c52ecdd5983f5620a4d519db395
Nikita <bletigri@gmail.com>
2024-07-27 19:14:58 +0300

    fix bugs

--- index.js
+++ index.js
@@ -52,3 +52,55 @@ import axios from "axios";
 // const reqREsponse = req.data
 // console.log(reqREsponse)
 //----------------------------------------------------------------------------
+
+
+const dataAuth = {
+    "username": "NikitaPups123",
+    "password": "ZXCPUDGE228"
+};
+
+const getTokens = async (form) => {
+    let error = null;
+    let tokens = {};
+
+    try {
+        const response = await axios.post('http://127.0.0.1:8000/api/v8/token/login/', form);
+
+        tokens = response.data;
+
+    } catch (errorr) {
+        error = errorr;
+    }
+    if (error) {
+        return 'Error. Check form please';
+    }
+
+    return tokens; 
+};
+
+export const login = async (form) => {
+    
+    const nextMessage = await getTokens(form);
+
+    if (typeof nextMessage === 'string') {
+        return nextMessage; 
+    }
+
+    const data = {};
+
+    const auth = {
+        'Authorization': `JWT ${nextMessage.access}`
+    };
+
+    try {
+        const response = await axios.get('http://127.0.0.1:8000/api/v8/auth/users/me', { headers: auth });
+        data.user = response.data; // Получаем информацию о пользователе
+    } catch (error) {
+        return 'Error fetching user data.';
+    }
+
+    return data.user; // Возвращаем информацию о пользователе
+};
+
+// Пример вызова
+login(dataAuth).then(message => console.log(message))
